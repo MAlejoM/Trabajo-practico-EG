e336b02fac004cc51ba14d523b14e5f527f1935e
Lumansito <luhmannm0@gmail.com>
2024-01-11 21:31:31 -0300

    Atenciones con visualizacion de mascota

--- codigo/consultarAtenciones.php
+++ codigo/consultarAtenciones.php
@@ -10,7 +10,31 @@ include_once("funciones.php");
             include("menuLateral.php")
         ?>
     </div>
+    <div>
+        <?php
+            $query = "SELECT * FROM mascotas WHERE id=".$_GET['id_mascota']; //consulta a la base de datos
+            $resultados = consultaSql($query); 
+            
+            foreach ($resultados as $mascota) { //recorre los resultados de la consulta
+    
+                echo "<div class='etiquetaMascota'>";
+                echo "<h3>".$mascota['nombre']."</h3>";
+                echo "<br>";
+                echo "<img src='data:image/jpeg;base64,".base64_encode($mascota['imagen'])."' />"; //imagen de la mascota
+                echo "<br>";
+                echo "<h4>".$mascota['color']."</h4>";
+                echo "<br>";
+                echo "<h4>".$mascota['raza']."</h4>";
+                echo "<br>";
+                echo "<h4>".$mascota['sexo']."</h4>";
+                echo "<br>";                
+                echo "</div>";
+            }
 
+
+
+        ?>
+    </div>
     <div class="atenciones">
     <?php
 
