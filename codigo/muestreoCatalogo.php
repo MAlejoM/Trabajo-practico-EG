32b0e7500b0ba48543e997646d38763ed1317d7c
Lumansito <luhmannm0@gmail.com>
2024-01-13 15:03:13 -0300

    update css

--- codigo/muestreoCatalogo.php
+++ codigo/muestreoCatalogo.php
@@ -6,7 +6,7 @@ $result = consultaSql( $query);
 foreach ($result as $row) {
     echo "<div class='card'> ";
    
-    echo "<img src='data:image/jpeg;base64,".base64_encode($row['imagen'])."' class='card-img-top' alt='".$row['nombre']."'  style='max-height: 200px'/>";
+    echo "<img src='data:image/jpeg;base64,".base64_encode($row['imagen'])."' class='card-img-top' alt='".$row['nombre']."'  style='height:200px ; object-fit: cover;'/>";
     echo "<div class='card-body'>";
     echo "<p class='card-text'>hola</p>";
     echo "</div>";
