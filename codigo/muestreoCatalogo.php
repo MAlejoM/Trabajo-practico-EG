68b8b751b5705d54ba106ff5365b11bd791b7a22
Lumansito <luhmannm0@gmail.com>
2024-01-13 14:49:30 -0300

    Arreglo style

--- codigo/muestreoCatalogo.php
+++ codigo/muestreoCatalogo.php
@@ -4,11 +4,12 @@ include_once("funciones.php");
 $query = "SELECT * FROM catalogo";
 $result = consultaSql( $query);
 foreach ($result as $row) {
-    echo "<div class='card'>
-    <img src='data:image/jpeg;base64'".base64_encode($row['imagen'])." class='card-img-top' alt='...'/>
-    <div class='card-body'>
-      <p class='card-text'>hola</p>
-    </div>";
+    echo "<div class='card'> ";
+   
+    echo "<img src='data:image/jpeg;base64,".base64_encode($row['imagen'])."' class='card-img-top' alt='".$row['nombre']."'  style='max-height: 200px'/>";
+    echo "<div class='card-body'>";
+    echo "<p class='card-text'>hola</p>";
+    echo "</div>";
     echo $row['nombre'];
     echo $row['descripcion'];
     echo $row['precio'];
