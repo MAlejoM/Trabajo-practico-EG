649d9d3fe885a2ac348f7cb432464cda6cd81a80
Matias Luhmann <139171817+Lumansito@users.noreply.github.com>
2026-03-28 17:01:36 -0300

    ajustamos generacion de bd

--- init.sql
+++ init.sql
@@ -177,7 +177,7 @@ CREATE TABLE `productos` (
   `nombre`      VARCHAR(200)   NOT NULL,
   `descripcion` TEXT           DEFAULT NULL,
   `precio`      DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
-  `imagen`      VARCHAR(255)   DEFAULT NULL,
+  `imagen`      LONGBLOB  DEFAULT NULL,
   `categoria`   VARCHAR(100)   DEFAULT NULL,
   `stock`       INT(11)        NOT NULL DEFAULT 0,
   `usuarioId`   INT(11)        NOT NULL,
